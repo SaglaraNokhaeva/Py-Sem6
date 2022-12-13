17c4303cc407bcf1a6228374f991f397a982db65
Saglara <nohaevass@yandex.ru>
2022-12-13 21:48:55 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,3 +3,17 @@
 # Результат отобразить на экране в виде последовательности оставшихся чисел в одну строчку через пробел.
 
 # пример - 8 11 0 -23 140 1 => 11 -23
+
+string_num=input("Введите числа через пробел: ").split()
+print(string_num)
+string_num=filter(lambda i: len(str(abs(int(i))))==2,string_num)
+string_num=list(string_num)
+print(string_num)
+
+
+# Или более кратко
+# string_num=list(filter(lambda i: len(abs(i))==2,input("Введите числа через пробел: ").split()))
+# print(string_num)
+
+# ДРугой вариант решения ? не работает?
+string_num=list(map(input("Введите числа через пробел: ").split()))
